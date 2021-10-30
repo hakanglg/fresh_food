391bd4edc9c284c5ebe072afc533441d5f3ee8c5
hakanglg <hakan_golge@hotmail.com>
2021-10-30 22:59:38 +0300

    Update fresh_food_view.dart

--- lib/fresh_food_view.dart
+++ lib/fresh_food_view.dart
@@ -12,38 +12,85 @@ class _FreshFoodViewState extends State<FreshFoodView> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          appBarTitle,
-          style: TextStyle(color: Colors.black),
-        ),
-        centerTitle: true,
-        backgroundColor: Colors.white,
-      ),
-      body: null,
-      floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: Icon(Icons.add),
-        backgroundColor: Colors.grey[700],
-      ),
+      appBar: AppBarM(),
+      body: BodyCards(context),
+      floatingActionButton: FloatActButton(),
       floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
-      bottomNavigationBar: BottomNavigationBar(
-        items: [
-          BottomNavigationBarItem(
-            icon: Icon(Icons.store_mall_directory_outlined),
-            title: Text(""),
-          ),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.quick_contacts_dialer_outlined),
-              title: Text("")),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_basket_outlined), title: Text("")),
-          BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text("")),
-        ],
-        backgroundColor: Colors.black,
-        unselectedItemColor: Colors.blueGrey,
-        fixedColor: Colors.black,
+      bottomNavigationBar: BottomNavBar(),
+    );
+  }
+
+  AppBar AppBarM() {
+    return AppBar(
+      elevation: 0,
+      title: Text(
+        appBarTitle,
+        style: Theme.of(context).textTheme.headline5,
       ),
+      centerTitle: true,
+      backgroundColor: Colors.white,
+    );
+  }
+
+  Column BodyCards(BuildContext context) {
+    return Column(
+      children: [
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SizedBox(
+            height: MediaQuery.of(context).size.height * 0.5,
+            child: ListView.builder(
+              itemCount: 11,
+              itemBuilder: (BuildContext context, int index) {
+                return Card(
+                  margin: EdgeInsets.fromLTRB(25, 15, 25, 5),
+                  color: Colors.grey,
+                  child: Container(
+                      height: MediaQuery.of(context).size.height * 0.07,
+                      child: Text("data")),
+                );
+              },
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: ElevatedButton(
+              onPressed: () {},
+              child: Text("CHECKOUT"),
+              style: ElevatedButton.styleFrom(
+                primary: Colors.green, // background
+                onPrimary: Colors.white,
+              )),
+        ),
+      ],
+    );
+  }
+
+  FloatingActionButton FloatActButton() {
+    return FloatingActionButton(
+      onPressed: () {},
+      child: Icon(Icons.add),
+      backgroundColor: Colors.grey[700],
+    );
+  }
+
+  BottomNavigationBar BottomNavBar() {
+    return BottomNavigationBar(
+      items: [
+        BottomNavigationBarItem(
+          icon: Icon(Icons.store_mall_directory_outlined),
+          title: Text(""),
+        ),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.quick_contacts_dialer_outlined), title: Text("")),
+        BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_basket_outlined), title: Text("")),
+        BottomNavigationBarItem(icon: Icon(Icons.settings), title: Text("")),
+      ],
+      backgroundColor: Colors.black,
+      unselectedItemColor: Colors.blueGrey,
+      fixedColor: Colors.black,
     );
   }
 }
